4c2f65e590aad9a32e5f67a893ea6bebc52aee3c
Emil Orvik Olsson <emil.olsson@propulsentnu.no>
2024-01-03 23:43:40 +0100

    Initial commit

---
+++ workout-log/src/App.tsx
@@ -0,0 +1,12 @@
+import Message from "./Message.tsx";
+import Footer from "./components/Footer.tsx";
+
+function App(){
+    return <div>
+        <Message></Message>
+        <Footer></Footer>
+    </div>
+
+}
+
+export default App;
